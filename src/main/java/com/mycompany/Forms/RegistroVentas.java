831e889e0e986c2b7eaca4d90354856b41dc60cf
pabloxolis <pabloxolis@pabloxolis-IdeaPad>
2025-06-14 00:23:19 -0600

    Aplicación y validación de cupones por monto y porcentaje en ventas.

--- src/main/java/com/mycompany/Forms/RegistroVentas.java
+++ src/main/java/com/mycompany/Forms/RegistroVentas.java
@@ -140,7 +140,7 @@ public class RegistroVentas extends javax.swing.JFrame {
                 .addGap(14, 14, 14)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel7)
                 .addGap(18, 18, 18)
